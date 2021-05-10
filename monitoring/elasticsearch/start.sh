0c408630b19ba7103bb95ed1424b8a1f6c8a4a9e
riteshkumar93 <riteshkumar442@gmail.com>
2021-05-10 12:36:30 +0530

    Added monitoring stuff

---
+++ monitoring/elasticsearch/start.sh
@@ -0,0 +1,4 @@
+curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.12.1-linux-x86_64.tar.gz
+tar -xzvf elasticsearch-7.12.1-linux-x86_64.tar.gz
+cd elasticsearch-7.12.1
+./bin/elasticsearch
---
+++ monitoring/kibana/start.sh
@@ -0,0 +1,4 @@
+curl -L -O https://artifacts.elastic.co/downloads/kibana/kibana-7.12.1-linux-x86_64.tar.gz
+tar xzvf kibana-7.12.1-linux-x86_64.tar.gz
+cd kibana-7.12.1-linux-x86_64/
+./bin/kibana
